a4b2d829a227f26e52b0009f939502ef2fd21288
nddat1811 <nddat1811@gmail.com>
2023-04-18 14:36:02 +0700

    oke

---
+++ server/sendgmail.py
@@ -0,0 +1,27 @@
+import base64
+from email.mime.text import MIMEText
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from requests import HTTPError
+
+SCOPES = [
+        "https://www.googleapis.com/auth/gmail.send"
+    ]
+flow = InstalledAppFlow.from_client_secrets_file('C:/Users/Admin/Documents/GitHub/remote-control/server/credentials.json', SCOPES)
+creds = flow.run_local_server(port=0)
+
+service = build('gmail', 'v1', credentials=creds)
+message = MIMEText('This is the body of the email')
+message['to'] = 'testpython18mmt@gmail.com'
+message['subject'] = 'test'
+create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+try:
+    message = (service.users().messages().send(userId="me", body=create_message).execute())
+    print(F'sent message to {message} Message Id: {message["id"]}')
+except HTTPError as error:
+    print(F'An error occurred: {error}')
+    message = None
+
+
+#https://mailtrap.io/blog/python-send-email-gmail/
